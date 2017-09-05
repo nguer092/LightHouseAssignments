cfc994628bb2dd7816b4378ea76f08dfa8b40b96
Nick Guerrero <nicolasjguerrero@gmail.com>
2017-09-04 21:11:16 -0400

    Full round, missing loop

--- WordEffects/WordEffects/main.m
+++ WordEffects/WordEffects/main.m
@@ -11,54 +11,87 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
 
-        
+        int choice;
+        BOOL play = YES;
         char inputChars [255];
         
-        NSString *inputString = @"Input a string: ";
+        while (play) {
+            
+        NSString *inputString = @"\nInput a string: ";
         
         NSLog(@"%@", inputString);
         
         fgets(inputChars, 255, stdin);
         
-        printf("Your string is %s\n", inputChars);
+        printf("\nYour string is %s\n", inputChars);
         
-//        Convert char array into an NSString object
+        printf("What would you like to do with this string?\n");
+        printf("Make it LOUDER - Type 1\n");
+        printf("make it smaller - Type 2\n");
+        printf("Numberize it - Type 3\n");
+        printf("Canadianize it - Type 4\n");
+        printf("See a response - Type 5\n");
+        printf("De-space it - Type 6\n");
+        scanf("%d", &choice);
+        
+        
+        //        Convert char array into an NSString object
         
         NSString *inputStringObject = [NSString stringWithUTF8String:inputChars];
         
-//        Uppercase
+        //        Uppercase
         
         NSString *capitalized = [inputStringObject uppercaseString];
-        NSLog(@"Your LOUD STRING IS %@", capitalized);
         
-//        Lowercase
+        
+        //        Lowercase
         
         NSString *lower = [inputStringObject lowercaseString];
-        NSLog(@"Your smaller string is %@", lower);
         
-//        Numberize
+        
+        //        Numberize
         
         NSInteger numberize = [inputStringObject intValue];
-        NSLog(@"Your numberized string is %ld", numberize);
         
-//        Canadianize
+        
+        //        Canadianize
         NSString *canadian = [inputStringObject stringByAppendingString:@"eh?"];
-        NSLog(@"Your canadianized string is %@", canadian);
-        
-//        Response
         
         
-//        De-space it
+        //        Response
+
+        
+        //        De-space it
         NSString *despaced = [inputStringObject stringByReplacingOccurrencesOfString:@" " withString:@"-"];
-        NSLog(@"Your despaced string is %@", despaced);
         
-//        printf("What would you like to do with this string?\n");
-//        printf("Make it LOUDER\n");
-//        printf("make it smaller\n");
-//        printf("Numberize it\n");
-//        printf("Canadianize it\n");
-//        printf("See a response\n");
-//        printf("De-space it\n");
+        
+        
+        switch (choice) {
+            case 1:
+            NSLog(@"Your LOUD STRING IS %@", capitalized);
+            break;
+            case 2:
+            NSLog(@"Your smaller string is %@", lower);
+            break;
+            case 3:
+            NSLog(@"Your numberized string is %ld", numberize);
+            case 4:
+            NSLog(@"Your canadianized string is %@", canadian);
+            case 5:
+            if ([inputStringObject containsString:@"?"]){
+                NSLog(@"I dont know.");
+            } else if  ([inputStringObject containsString:@"!"]) {
+                NSLog(@" Woah buddy, calm down!");
+            };
+            case 6:
+            NSLog(@"Your despaced string is %@", despaced);
+            break;
+            default:
+            break;
+        }
+            
+        }
+
         
         
         
